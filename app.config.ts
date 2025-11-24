6b3a83b392b062b1d8cf7a5f97f334ebb11549e6
Stanley Xu <stanley.xuu@gmail.com>
2025-11-24 12:23:41 -0500

    config: iOS only

--- app.config.ts
+++ app.config.ts
@@ -7,26 +7,15 @@ export default {
     slug: "giftful",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/icons/icon.png",
+    icon: "./assets/giftful.icon",
     scheme: "giftful",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
+    platforms: ["ios"],
     ios: {
       icon: "./assets/giftful.icon",
       bundleIdentifier: "io.giftful",
       supportsTablet: true,
-    },
-    android: {
-      adaptiveIcon: {
-        foregroundImage: "./assets/images/adaptive-icon.png",
-        backgroundColor: "#ffffff",
-      },
-      edgeToEdgeEnabled: true,
-    },
-    web: {
-      bundler: "metro",
-      output: "static",
-      favicon: "./assets/images/favicon.png",
     },
     plugins: [
       "expo-router",
